3771bc9e03a0c8ffaed01ab914758308df4686ce
Rosy Patel <rosypatel109@hotmail.com>
2022-05-09 15:45:37 +0100

    set up staffs view controller

--- DirectoryApp/Staffs/View/StaffsViewController.swift
+++ DirectoryApp/Staffs/View/StaffsViewController.swift
@@ -9,21 +9,70 @@ import UIKit
 
 class StaffsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var collectionView: UICollectionView!
+    private var staffViewModel: StaffsViewModel = StaffsViewModel()
+    
+    private var columnCount: Int {
+      switch UIDevice.current.userInterfaceIdiom {
+      case .pad:
+        return Constants.iPadColumnCount
+      default:
+        return Constants.iPhoneColumnCount
+      }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private let minimumInteritemSpacing: CGFloat = 10.0
+    
+    lazy var flowLayout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = minimumInteritemSpacing
+        return layout
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        staffViewModel.delegate = self
+        staffViewModel.getStaffInfo()
     }
-    */
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.collectionView.collectionViewLayout = flowLayout
+    }
+}
 
+extension StaffsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return staffViewModel.itemCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "staffsViewCell", for: indexPath) as? StaffViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let staffDetails = staffViewModel.getStaffNameAndImageId(for: indexPath.row)
+        cell.setData(staffDetails: staffDetails)
+        return cell
+    }
+}
+
+extension StaffsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sideInsets = collectionView.safeAreaInsets.left + collectionView.safeAreaInsets.right
+        let spaceBetween = minimumInteritemSpacing * CGFloat(columnCount - 1)
+        
+        let cellWidth = (collectionView.bounds.width - (sideInsets + spaceBetween)) / CGFloat(columnCount)
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
+}
+
+extension StaffsViewController: StaffViewModelDelegate {
+    func updateUI() {
+        collectionView.reloadData()
+    }
+    
+    func showError() {
+        collectionView.reloadData()
+    }
 }
--- DirectoryApp/Utility/Constants.swift
+++ DirectoryApp/Utility/Constants.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 struct Constants {
+    static let  iPhoneColumnCount = 2
+    static let  iPadColumnCount = 4
     static let  urlNotCorrect = "URL is not correct"
     static let  noResponse = "Failed to get response"
     static let  noData = "Failed to get Data"
